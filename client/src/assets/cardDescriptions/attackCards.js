0bd909436288288eca2d2e861a71780acff8f594
vertefra <verte.fra@gmail.com>
2021-01-31 11:17:45 -0500

    ok, now for real

--- client/src/assets/cardDescriptions/attackCards.js
+++ client/src/assets/cardDescriptions/attackCards.js
@@ -28,3 +28,39 @@ Hit List.
 export const stValentinedescription = `
 The St. Valentine’s Day Massacre immediately eliminates all mobsters on the Hit List.
 `
+
+export const doubleCrossdescription = `
+The Double Cross card allows you to collect 1 mobster from each opponent and add
+them all to the Hit List in any order that you choose.
+`
+
+export const mobWarDescription =`
+Mob War starts (or restarts at a normal rate) a Mob War, even if there are less than
+6 mobsters on the Hit List.
+`
+
+export const ambushDescription = `
+An Ambush starts a Mob War, with 2 mobsters killed at the beginning of each player’s
+turn. This continues until the Mob War ends. (If a Mob War is already in effect, this
+has the effect of doubling the hit rate.)
+`
+
+export const vendettaDescription = `
+A Vendetta allows you to select 2 mobsters from each opponent and add them all to
+the Hit List, in any order you choose. Then, a Mob War starts (or continues, if a Mob
+War was already in effect), with 2 mobsters killed at the beginning of each player’s
+turn. This continues until the Mob War ends. If you use a Safe House counter card
+when Vendetta is played, your mobsters are not added to the Hit List, but the Mob
+War still begins.
+`
+
+export const turnCoatDescription = `
+The Turncoat lets you switch a mobster in play (on the Hit List or in front of a player)
+with a mobster from the discard pile. The mobster being discarded must come from
+the player with the most mobsters in play, and the returned mobster must be for the
+active player with the least mobsters in play. In the case of a tie, you choose which of
+the tied players are affected. The returned mobster comes back to the same spot on
+the Hit List that the discarded mobster occupied, or is returned in front of the player if
+the discarded mobster was not on the Hit List. This card does not bring an eliminated
+player back into the game.
+`
